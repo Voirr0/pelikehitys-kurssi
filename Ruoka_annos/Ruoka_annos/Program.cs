c90b665e91830055f1215513061e514a2c5eb832
s2401133 <s2401133@edu.bc.fi>
2026-02-23 10:13:25 +0200

    Ruoka_annos
    Ruoka_annos tehtävä valmis!

---
+++ Ruoka_annos/Ruoka_annos/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace Ruoka_annos
+{
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            ateria tilaus = new ateria();
+
+            string[] Pääraaka_aine = Enum.GetNames<Pääraaka_aine>();
+            Console.WriteLine("Valitse pääraaka-aine");
+            for (int i = 0; i < Pääraaka_aine.Length; i++)
+            {
+                Console.WriteLine(Pääraaka_aine[i]);
+            }
+
+            Console.WriteLine("-------------------------------------------------");
+                string vastaus = Console.ReadLine();
+            Pääraaka_aine valintaout;
+
+            if (Enum.TryParse<Pääraaka_aine>(vastaus,out valintaout))
+            {
+                tilaus.pääaine = valintaout;
+            }
+
+
+
+            string[] Lisuke = Enum.GetNames<Lisuke>();
+            Console.WriteLine("Valitse Lisuke");
+            for (int i = 0; i < Lisuke.Length; i++)
+            {
+                Console.WriteLine(Lisuke[i]);
+            }
+
+            Console.WriteLine("-------------------------------------------------");
+            string vastaus1 = Console.ReadLine();
+            Lisuke valintaout1;
+
+            if (Enum.TryParse<Lisuke>(vastaus, out valintaout1))
+            {
+                tilaus.lisuke = valintaout1;
+            }
+
+            string[] Kastike = Enum.GetNames<Kastike>();
+            Console.WriteLine("Valitse Kastike");
+            for (int i = 0; i < Lisuke.Length; i++)
+            {
+                Console.WriteLine(Kastike[i]);
+            }
+
+            Console.WriteLine("-------------------------------------------------");
+            string vastaus2 = Console.ReadLine();
+            Kastike valintaout2;
+
+            if (Enum.TryParse<Kastike>(vastaus, out valintaout2))
+            {
+                tilaus.kastike = valintaout2;
+            }
+
+            Console.WriteLine($"{vastaus} ja {vastaus1} {vastaus2}-kastikkeella");
+        }
+    }
+}
---
+++ Ruoka_annos/Ruoka_annos/ateria.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ruoka_annos
+{
+    
+
+    enum Pääraaka_aine
+    {
+        nautaa,
+        kanaa,
+        kasviksia
+        
+    }
+
+    enum Lisuke
+    {
+        perunaa,
+        riisiä,
+        pastaa,
+    }
+
+    enum Kastike
+    {
+        curry,
+        hapanimelä,
+        pippuri,
+        chili
+    }
+    internal class ateria
+    {
+        public Pääraaka_aine pääaine;
+        public Lisuke lisuke;
+        public Kastike kastike;
+
+    }
+}
